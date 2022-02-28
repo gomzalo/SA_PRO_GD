fb19e0a0aa3a99657cebc78e674c10852806e387
A <201318652@ingenieria.usac.edu.gt>
2022-02-27 23:15:28 -0600

    Registro con confirmacion de correo.

--- backend/user/api/controllers/user_controller.js
+++ backend/user/api/controllers/user_controller.js
@@ -1,4 +1,5 @@
-const userm = require('../models/user_model')
+const userm = require('../models/user_model');
+const { get_last_id, email_confirmation } = require('../models/user_model');
 
 module.exports = {
     all: function(req, res) {
@@ -6,19 +7,17 @@ module.exports = {
         res.status(200).send(rows);
       })
     },
-  
 
-    add: function(req, res) {
-      userm.add(req.con, req.body, function(err, rows){
-        console.log("ROWSSSS");
-        console.log(rows);
+    add: async function(req, res) {
+      userm.add(req.con, req.body, async function(err, rows){
         if(err){
-          // console.log(res)
           res.status(409).send({
             status: false,
-            msj: 'Error al crear usuario'
+            msj: 'Error al crear usuario',
+            error: err.toString()
           });
         } else {
+          userm.send_email({email: req.body.email, id: rows.insertId});
           res.status(200).send({
             status: true,
             msj: 'Usuario creado con exito'
@@ -27,7 +26,54 @@ module.exports = {
       });  
     },
     
-    validar_email(correo){
+    email_confirmation: function(req, res)   {
+      userm.email_confirmation(req.con, req.query.id, function(err, rows){
+        if(err){
+          res.status(409).send(
+            `
+            <!doctype html>
+            <html lang="en">
+              <head>
+                  <title>Soccer Stats SIUUU</title>
+                  <meta name="viewport" content="width=device-width, initial-scale=1">
+                  <link rel="stylesheet" href="https://bootswatch.com/5/vapor/bootstrap.css">
+                  <meta charset="utf-8" />
+              </head>
+              <body>
+                <center>
+                  <h1>¡No se ha podido confirmar su cuenta!</h1>
+                  Puede que ya lo hayas hecho.
+                  <a href="http://0.0.0.0:4200/login">Iniciar sesión</a>
+                </center>
+              </body>
+            </html>
+                `
+          );
+        } else {
+          res.status(200).send(
+            `
+            <!doctype html>
+            <html lang="en">
+              <head>
+                <title>Soccer Stats SIUUU</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="stylesheet" href="https://bootswatch.com/5/vapor/bootstrap.css">
+                <meta charset="utf-8" />
+              </head>
+              <body>
+                <center>
+                  <h1>¡Se ha confirmado su cuenta!</h1>
+                  <a href="http://0.0.0.0:4200/login">Iniciar sesión</a>
+                </center>
+              </body>
+            </html>
+                `
+          );
+        }
+      });
+    },
+
+    validar_email(correo) {
       if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(correo)) {
         return true;
       }
--- backend/user/api/routes/user_route.js
+++ backend/user/api/routes/user_route.js
@@ -4,5 +4,6 @@ const user_controller = require('../controllers/user_controller');
 
 router.get('/all', user_controller.all);
 router.post('/add', user_controller.add);
+router.get('/email_confirmation', user_controller.email_confirmation);
 
 module.exports = router

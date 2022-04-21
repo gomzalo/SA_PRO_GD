5071cfe30874450b50a88ad14572a4119f078168
A <201318652@ingenieria.usac.edu.gt>
2022-04-21 00:44:03 -0600

    reportes administrador 1-5

--- client/src/app/pages/admin/reportes/reportes.component.ts
+++ client/src/app/pages/admin/reportes/reportes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { withLatestFrom } from 'rxjs/operators';
 import { EquipoService } from 'src/app/shared/services/equipo.service';
+import { PaisService } from 'src/app/shared/services/pais.service';
 import { ReportesService } from 'src/app/shared/services/reportes.service';
 
 @Component({
@@ -13,9 +14,14 @@ export class ReportesComponent implements OnInit {
   selectedReporte = 0;
   equipos
   equipo = 0
+  paises
+  pais = 0
   edad = 0
   gender = ''
   orden = -1
+  id_country = 0
+  id_team = 0
+  membership = 0
   report_data = {}
   genero = [{ id: 'M', value: 'Masculino' }, { id: 'F', value: 'Femenino' }, { id: 'U', value: 'Lo que sea' },]
   reportes = [
@@ -35,30 +41,36 @@ export class ReportesComponent implements OnInit {
     this.equipoService.getAllteams()
       .subscribe((data) => { this.equipos = data.data; console.log(this.equipos) });
   }
-  constructor(private equipoService: EquipoService, private reportesService: ReportesService, private router: Router) {
 
+  getpaises() {
+    this.paisService.getpaises()
+      .subscribe((data) => { this.paises = data.data; console.log(this.paises) });
+  }
+
+  constructor(private equipoService: EquipoService, private paisService: PaisService, private reportesService: ReportesService, private router: Router) {
   }
 
   ngOnInit(): void {
     this.getequipos();
+    this.getpaises();
   }
 
   getdata() {
     switch (this.selectedReporte) {
       case 1:
-
+        this.getReporte1();
         break;
       case 2:
-
+        this.getReporte2();
         break;
       case 3:
-
+        this.getReporte3();
         break;
       case 4:
-
+        this.getReporte4();
         break;
       case 5:
-
+        this.getReporte5();
         break;
       case 6:
         this.getReporte6();
@@ -81,6 +93,71 @@ export class ReportesComponent implements OnInit {
     }
   }
 
+  getReporte1() {
+    this.reportesService.reporte1(this.equipo)
+      .subscribe(
+        (data) => { this.report_data = data.data; console.log(this.report_data); }, 
+        error => {
+          console.log(error)
+          if (error.status== 401) {
+            this.router.navigate(['unauthorized']);
+          }
+        })
+      ;
+  }
+
+  getReporte2() {
+    this.reportesService.reporte2(this.membership)
+      .subscribe(
+        (data) => { this.report_data = data.data; console.log(this.report_data); }, 
+        error => {
+          console.log(error)
+          if (error.status== 401) {
+            this.router.navigate(['unauthorized']);
+          }
+        })
+      ;
+  }
+
+  getReporte3() {
+    this.reportesService.reporte3()
+      .subscribe(
+        (data) => { this.report_data = data.data; console.log(this.report_data); }, 
+        error => {
+          console.log(error)
+          if (error.status== 401) {
+            this.router.navigate(['unauthorized']);
+          }
+        })
+      ;
+  }
+
+  getReporte4() {
+    this.reportesService.reporte4()
+      .subscribe(
+        (data) => { this.report_data = data.data; console.log(this.report_data); }, 
+        error => {
+          console.log(error)
+          if (error.status== 401) {
+            this.router.navigate(['unauthorized']);
+          }
+        })
+      ;
+  }
+
+  getReporte5() {
+    this.reportesService.reporte5(this.id_country)
+      .subscribe(
+        (data) => { this.report_data = data.data; console.log(this.report_data); }, 
+        error => {
+          console.log(error)
+          if (error.status== 401) {
+            this.router.navigate(['unauthorized']);
+          }
+        })
+      ;
+  }
+
   getReporte6() {
     this.reportesService.reporte6(this.gender)
       .subscribe(
@@ -93,6 +170,7 @@ export class ReportesComponent implements OnInit {
         })
       ;
   }
+
   getReporte7() {
     this.reportesService.reporte7(this.edad)
       .subscribe((data) => { this.report_data = data.data; console.log(this.report_data); },
--- client/src/app/shared/services/reportes.service.ts
+++ client/src/app/shared/services/reportes.service.ts
@@ -17,30 +17,47 @@ export class ReportesService {
   headerDict = {
     'Authorization': 'Bearer '+JSON.parse(localStorage.getItem('currentUser')).token,
   }
-  reporte1() { }
-  reporte2() { }
-  reporte3() { }
-  reporte4() { }
-  reporte5() { }
+
+  reporte1(id_team:Number) {
+    return this.http.get<any>(environment.apiAdministrador+'report/1?id_team='+id_team,{headers: new HttpHeaders(this.headerDict)})
+  }
+
+  reporte2(membership:Number) { 
+    return this.http.get<any>(environment.apiAdministrador+'report/2?membership='+membership,{headers: new HttpHeaders(this.headerDict)})
+  }
+
+  reporte3() { 
+    return this.http.get<any>(environment.apiAdministrador+'report/3',{headers: new HttpHeaders(this.headerDict)})
+  }
+
+  reporte4() {
+    return this.http.get<any>(environment.apiAdministrador+'report/4',{headers: new HttpHeaders(this.headerDict)})
+  }
+
+  reporte5(id_country:Number) {
+    return this.http.get<any>(environment.apiAdministrador+'report/5?id_country='+id_country,{headers: new HttpHeaders(this.headerDict)})
+  }
 
   reporte6(genero:string) {
     return this.http.get<any>(environment.apiAdministrador+'report/6?gender='+genero,{headers: new HttpHeaders(this.headerDict)})
-   }
+  }
 
   reporte7(age:Number) {
     return this.http.get<any>(environment.apiAdministrador+'report/7?age='+age,{headers: new HttpHeaders(this.headerDict)});
-   }
+  }
 
   reporte8(order:Number) {
     return this.http.get<any>(environment.apiAdministrador+'report/8?order='+order,{headers: new HttpHeaders(this.headerDict)});
-   }
+  }
+
   reporte9(order:Number,id_team:Number) { 
     return this.http.get<any>(environment.apiAdministrador+'report/9?order='+order+'&id_team='+id_team,{headers: new HttpHeaders(this.headerDict)});
   }
+
   reporte10() {
     return this.http.get<any>(environment.apiAdministrador+'report/10',{headers: new HttpHeaders(this.headerDict)});
   }
-   
+
   private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
